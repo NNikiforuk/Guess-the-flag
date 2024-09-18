f7f3f0715e0127e1e9b38bcb60c9c572bc5a28bd
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-09-18 18:40:31 +0200

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Natalia Nikiforuk on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
